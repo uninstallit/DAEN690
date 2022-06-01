0bdec8d5535351a981f8714cbfd4945f97e1b8a9
tranbre <brenda.tran@saic.com>
2022-06-01 18:14:00 -0400

    cleanup

--- src/functions/import_sqlite.py
+++ src/functions/import_sqlite.py
@@ -23,27 +23,27 @@ def import_notams_table (conn,  file_name):
         print(format_exc())
         
 def import_sample_data():
-    sample_db = '../sample_data/sample.db'
+    sample_db = '../../sample_data/sample.db'
     file = Path(sample_db)
     file.touch(exist_ok=True)
     conn = sqlite3.connect(sample_db)
-    import_launches_table(conn, '../sample_data/LAUNCHES_sample.csv')
-    import_spaceports_table(conn, '../sample_data/SPACEPORTS_sample.csv')
-    import_notams_table(conn, '../sample_data/NOTAM_sample.csv')
+    import_launches_table(conn, '../../sample_data/LAUNCHES_sample.csv')
+    import_spaceports_table(conn, '../../sample_data/SPACEPORTS_sample.csv')
+    import_notams_table(conn, '../../sample_data/NOTAM_sample.csv')
     conn.close()
 
 def import_svo_data():
-    svo_db = '../data/svo_db_20200901.db'
+    svo_db = '../../data/svo_db_20200901.db'
     file = Path(svo_db)
     file.touch(exist_ok=True)
     conn = sqlite3.connect(svo_db)
-    import_launches_table(conn, '../data/launches.csv')
-    import_spaceports_table(conn, '../data/spaceports.csv')
-    import_notams_table(conn, '../data/notams_v3.csv')
+    import_launches_table(conn, '../../data/launches.csv')
+    import_spaceports_table(conn, '../../data/spaceports.csv')
+    import_notams_table(conn, '../../data/notams_v3.csv')
     conn.close()
 
-#import_sample_data()
-import_svo_data()
+import_sample_data()
+#import_svo_data()
 
 
 
